6f88aa0f502eaa99a051daad904dd265da3d00a1
John Orr <john.orr@bearingpoint.com>
2017-03-24 15:04:12 +0000

    removing the functionality which calculates the period covered

--- src/com/dev/Main.java
+++ src/com/dev/Main.java
@@ -53,15 +53,12 @@ public class Main {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_FORMAT);
 
         logEntries = new ArrayList<>();
-        Map<String, Date> firstDateForCluster = new HashMap<>();
-        Map<String, Date> lastDateForCluster = new HashMap<>();
         for (File logFile : logFiles) {
             BufferedReader reader = new BufferedReader(new FileReader(logFile));
             LOG.info("Reading from file: " + logFile);
             String cluster = getCluster(logFile);
             String line;
             LogEntry logEntry = null;
-            Date date = null;
             while ((line = reader.readLine()) != null) {
                 Matcher datePatternMatcher = DATE_PATTERN.matcher(line);
                 if (datePatternMatcher.find()) {
@@ -70,11 +67,8 @@ public class Main {
                         logEntries.add(logEntry);
                     }
                     // start new log entry
-                    date = simpleDateFormat.parse(line);
+                    Date date = simpleDateFormat.parse(line);
                     logEntry = new LogEntry(date, line, cluster);
-                    if (!firstDateForCluster.containsKey(cluster) || firstDateForCluster.get(cluster).after(date)) {
-                        firstDateForCluster.put(cluster, date);
-                    }
                 } else if (logEntry != null) {
                     // continuation of current entry
                     logEntry.append(line);
@@ -86,50 +80,29 @@ public class Main {
                     logEntry.setExclude();
                 }
             }
-            if (!lastDateForCluster.containsKey(cluster) || lastDateForCluster.get(cluster).before(date)) {
-                lastDateForCluster.put(cluster, date);
-            }
             // Write final LogEntry
             logEntries.add(logEntry);
         }
-        LOG.info("These files cover the period\n\t\t\t\t\t\t" + simpleDateFormat
-                .format(latestFirstDate(firstDateForCluster)) + " to\n\t\t\t\t\t\t" + simpleDateFormat
-                .format(earliestLastDate(lastDateForCluster)));
+        LOG.info(String.format("Read %d lines", logEntries.size()));
     }
 
     private static String getCluster(File logFile) {
         return logFile.getParent().substring(logFile.getParent().length()-1);
     }
 
-    private static Date earliestLastDate(Map<String, Date> lastDateForCluster) {
-        Date earliestLastDate = null;
-        for (Date lastDateOfCluster : lastDateForCluster.values()) {
-            if (earliestLastDate == null || earliestLastDate.after(lastDateOfCluster)) {
-                earliestLastDate = lastDateOfCluster;
-            }
-        }
-        return earliestLastDate;
-    }
-
-    private static Date latestFirstDate(Map<String, Date> firstDateForCluster) {
-        Date latestFirstDate = null;
-        for (Date firstDateOfCluster : firstDateForCluster.values()) {
-            if (latestFirstDate == null || latestFirstDate.before(firstDateOfCluster)) {
-                latestFirstDate = firstDateOfCluster;
-            }
-        }
-        return latestFirstDate;
-    }
-
     private static void writeLogs() throws IOException {
         Collections.sort(logEntries);
 
+        int excludeCount = 0;
         FileWriter writer = new FileWriter(OUTPUT_FILE);
         for (LogEntry logEntry : logEntries) {
             if (!logEntry.isExclude()) {
                 writer.write(logEntry.toString());
+            } else {
+                excludeCount++;
             }
         }
+        LOG.info(String.format("Excluded %d lines", excludeCount));
         writer.close();
     }
 }
